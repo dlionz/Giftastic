d21374c884b00d997a393c2b09251d182e2ff9a8
Damien <damienebreaux@gmail.com>
2018-03-13 11:43:35 -0500

    refactor and beautify

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-var topics = ["Dog", "Cat", "Lion", "Rabbit", "Goat", "Chicken", "Corgi"];
+var topics = ["Dog", "Cat", "Lion", "Rabbit", "Goat", "Corgi"];
 
 // Display the buttons on the screen
 function createButtons() {
@@ -6,7 +6,7 @@ function createButtons() {
 
   for (var i = 0; i < topics.length; i++) {
     var a = $("<button>");
-    a.addClass("animalBtn");
+    a.addClass("animalBtn btn btn-primary");
     a.attr("data-name", topics[i].toLowerCase());
     a.text(topics[i]);
     $("#animalButtons").append(a);
@@ -16,56 +16,60 @@ function createButtons() {
 //Creates new buttons for the page
 $("#addAnimal").on("click", function(event) {
   event.preventDefault();
-  var animal = $("#animal-input").val().trim();
-  topics.push(animal);
-  createButtons();
-  $("input[type='text']").val("");
+  var animal = $("#animal-input")
+    .val()
+    .trim();
+  if (animal != "") {
+    topics.push(animal);
+    createButtons();
+    $("input[type='text']").val("");
+  } else {
+    alert("Please enter a value");
+  }
 });
 
 //Displays the ainimal gifs on the page
 function displayAnimals() {
-
   $("#animals").empty();
   var animal = $(this).attr("data-name");
-  var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+  var queryUrl =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    animal +
+    "&api_key=dc6zaTOxFJmzC&limit=10";
 
   $.ajax({
     url: queryUrl,
     method: "GET"
-  }).done(function(response) {
+  }).done(function(res) {
     //puts still gifs on the page
-    var result = response.data;
+    var result = res.data;
 
     for (var i = 0; i < result.length; i++) {
-      console.log(response);
+      console.log(res);
       var imgUrl = result[i].images.fixed_height_still.url;
       var image = $("<img>").attr("src", imgUrl);
       image.attr("data-still", result[i].images.fixed_height_still.url);
       image.attr("data-animate", result[i].images.fixed_height.url);
       image.attr("data-state", "still");
-      image.addClass("animalClick");
+      image.addClass("animalClick card");
       $("#animals").append(image);
     }
-
   });
 }
 
 //click to animate --THIS FUNCTION--
 $(document).on("click", ".animalClick", function() {
-
   var state = $(this).attr("data-state");
 
-  if(state == "still"){
+  if (state == "still") {
     var url = $(this).attr("data-animate");
-    $(this).attr("data-state", 'animate');
+    $(this).attr("data-state", "animate");
     $(this).attr("src", url);
-  }
-  else{
+  } else {
     var url = $(this).attr("data-still");
     $(this).attr("data-state", "still");
     $(this).attr("src", url);
   }
-
 });
 
 // click listener for executing displayAnimals function
